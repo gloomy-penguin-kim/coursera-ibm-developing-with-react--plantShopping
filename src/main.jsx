b8f3fbe65e134e364d76d0ff7cee00d73f0966ed
gloomy.penguin.kim <gloomy.penguin.kim@gmail.com>
2024-11-17 13:01:14 -0600

    updated index.html

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render( 
     <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>,
+    </Provider> ,
 )
