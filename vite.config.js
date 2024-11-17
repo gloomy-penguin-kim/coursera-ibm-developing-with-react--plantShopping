b53b23da8f71852eb6ccd88bb7f28e1fe06c3375
gloomy.penguin.kim <gloomy.penguin.kim@gmail.com>
2024-11-17 13:11:23 -0600

    suggestion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "https://gloomy-penguin-kim.github.io/coursera-ibm-developing-with-react--plantShopping/",
   plugins: [react()],
 })
